b1f25cd7c41f53e693801be5062cc3a503b06ecf
Moritz Meyer <privat.moritzmeyer@googlemail.com>
2018-11-17 11:43:14 +0100

    changed targetFramework to 4.6.2

---
+++ Graph/Extensions/HashSetExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphCollection.Extensions
+{
+    public static class HashSetExtensions
+    {
+        #region TryGetValue
+        /// <summary>
+        /// Durchsucht das HashSet nach einem 'gleichen' Wert und gibt den gleichen Wert zurück, wenn es einene findet
+        /// </summary>
+        /// <typeparam name="T">Typ des Wertes.</typeparam>
+        /// <param name="hashSet">Das HashSet</param>
+        /// <param name="equalValue">Der Wert, nach dem gesucht wird.</param>
+        /// <param name="actualValue">Der gleiche wert, wenn vorhanden.</param>
+        /// <returns>True, wenn der Wert gefunden werden konnte, False wenn nicht.</returns>
+        public static bool TryGetValue<T>(this HashSet<T> hashSet, T equalValue, out T actualValue)
+        {
+            if (hashSet.Contains(equalValue))
+            {
+                int i = hashSet.ToList().IndexOf(equalValue);
+                actualValue = hashSet.ToList()[i];
+                return true;
+            }
+
+            actualValue = default(T);
+            return false;
+        }
+        #endregion
+    }
+}
---
+++ GraphCollectionTest/HashSetTest.cs
@@ -0,0 +1,42 @@
+﻿using GraphCollection;
+using GraphCollection.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphCollectionTest
+{
+    [TestClass]
+    public class HashSetTest
+    {
+        [TestMethod]
+        public void TryGetValue_must_work()
+        {
+            TwoBuckets tb1 = new TwoBuckets(3, 5, 2, 4);
+            TwoBuckets tb2 = new TwoBuckets(2, 7, 1, 5);
+            TwoBuckets tb3 = new TwoBuckets(4, 9, 3, 7);
+
+            HashSet<TwoBuckets> hashSet = new HashSet<TwoBuckets>
+            {
+                tb1,
+                tb2,
+                tb3
+            };
+
+            Assert.IsTrue(hashSet.TryGetValue(new TwoBuckets(3, 5, 2, 4), out TwoBuckets tb4));
+            Assert.AreEqual(tb1, tb4);
+            Assert.IsTrue(object.ReferenceEquals(tb1, tb4));
+
+            Assert.IsTrue(hashSet.TryGetValue(new TwoBuckets(2, 7, 1, 5), out TwoBuckets tb5));
+            Assert.AreEqual(tb2, tb5);
+            Assert.IsTrue(object.ReferenceEquals(tb2, tb5));
+
+            Assert.IsTrue(hashSet.TryGetValue(new TwoBuckets(4, 9, 3, 7), out TwoBuckets tb6));
+            Assert.AreEqual(tb3, tb6);
+            Assert.IsTrue(object.ReferenceEquals(tb3, tb6));
+        }
+    }
+}
